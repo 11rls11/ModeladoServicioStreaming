5357fdddf9217dfd0325b0f929204ce0d45073ff
Raymundo Lopez Santiago <raymundo.lopzantiago@outlook.com>
2025-06-12 17:42:41 -0600

    Changed realizarReseña to realizarResenia to avoid compiler problems

--- UI_UX.h
+++ UI_UX.h
@@ -15,7 +15,7 @@ class UI_UX {
         void mostrarMenuSubseleccion(const int &opcion, Video* &videos);
         void mostrarGenerosDisponibles(const int &opcion);
         void mostrarMenuAccion(const int &opcion, int &numero);
-        void realizarReseña(int &ID);
+        void realizarResenia(int &ID);
     public:
         UI_UX();
         void mostrarMenuPrincipal();
